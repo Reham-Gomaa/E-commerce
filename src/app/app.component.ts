c07f2ea5ecd9e87bc83f9a694a643f07b0b53f19
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-07-28 19:01:35 +0300

    refactor: 💡 routes
    add paths in app.routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './features/layouts/footer/footer.component';
+import { NavbarComponent } from './features/layouts/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gallery';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './features/pages/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  {
+    path: 'details/:p_id',
+    loadComponent: () =>
+      import('./features/pages/product-details/product-details.component').then(
+        (c) => c.ProductDetailsComponent
+      ),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./features/pages/cart/cart.component').then(
+        (c) => c.CartComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/pages/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+  },
+];
