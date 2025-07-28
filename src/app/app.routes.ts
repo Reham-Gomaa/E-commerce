bc94b0927267c9071172c21b6e1df93e64e94942
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-07-28 19:52:03 +0300

    feat: 🎸 build navbar
    build responsive navbar and optimize it for seo and accessibility

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { ProductsComponent } from './features/pages/products/products.component'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'products', component: ProductsComponent },
+  { path: 'home', component: ProductsComponent, title: 'Home' },
   {
     path: 'details/:p_id',
     loadComponent: () =>
       import('./features/pages/product-details/product-details.component').then(
         (c) => c.ProductDetailsComponent
       ),
+    title: 'Product Details',
   },
   {
     path: 'cart',
@@ -17,6 +18,7 @@ export const routes: Routes = [
       import('./features/pages/cart/cart.component').then(
         (c) => c.CartComponent
       ),
+    title: 'Cart',
   },
   {
     path: '**',
@@ -24,5 +26,6 @@ export const routes: Routes = [
       import('./features/pages/not-found/not-found.component').then(
         (c) => c.NotFoundComponent
       ),
+    title: 'Not Found',
   },
 ];
